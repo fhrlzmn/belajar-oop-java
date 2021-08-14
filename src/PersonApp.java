e2aa8d05c19db5b0eb10608ebe364ec55200dda4
fhrlzmn <fhrlzmn26@gmail.com>
2021-08-14 12:05:48 +0700

    Menambah field name, address, dan country pada class Person

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,5 +1,12 @@
 public class PersonApp {
     public static void main(String[] args) {
+        var Person = new Person();
 
+        Person.name = "Fahrul";
+        Person.address = "Bandung";
+
+        System.out.println(Person.name);
+        System.out.println(Person.address);
+        System.out.println(Person.country);
     }
 }
