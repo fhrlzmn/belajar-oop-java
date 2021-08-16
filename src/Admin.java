7a566e8938d90997c19dad62ed39e228b64b5b0e
fhrlzmn <fhrlzmn26@gmail.com>
2021-08-16 22:06:27 +0700

    Implementasi Super Constructor, membuat Constructor (Parameter) di Parent

--- src/Admin.java
+++ src/Admin.java
@@ -3,6 +3,12 @@
 class Admin {
     String name;
 
+    // Implementasi Super Constructor
+    // Menambah Constructor di Parent
+    Admin(String name) {
+        this.name = name;
+    }
+
     void sayHello(String name) {
         System.out.printf("Hello %s, Nama Saya Admin %s\n", name, this.name);
     }
@@ -11,6 +17,13 @@ class Admin {
 // Class turunan dari Admin
 // Class child punya field dan method yang dimiliki parent
 class UserOne extends Admin {
+
+    // Harus ditambah Constructor juga di Child (jika constructor parent ada parameter)
+    UserOne(String name) {
+        // dan Harus memanggil setidaknya 1 constructor yang ada di Parent
+        super(name);
+    }
+
     // Method Overriding
     void sayHello(String name) {
         System.out.printf("Hello %s, Nama Saya User %s\n", name, this.name);
--- src/AdminApp.java
+++ src/AdminApp.java
@@ -1,11 +1,9 @@
 public class AdminApp {
     public static void main(String[] args) {
-        var admin = new Admin();
-        admin.name = "Fahrul";
+        var admin = new Admin("Fahrul");
         admin.sayHello("Kawan");
 
-        var userOne = new UserOne();
-        userOne.name = "Fahrul";
+        var userOne = new UserOne("Fahrul");
         userOne.sayHello("Kawan");
     }
 }
